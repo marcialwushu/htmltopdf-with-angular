028b75140b2a17cc19790278c628f9c66d4145b7
marcialwushu <marcialwushu@gmail.com>
2018-12-13 17:24:04 -0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HtmltopdfComponent } from './htmltopdf/htmltopdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HtmltopdfComponent
   ],
   imports: [
     BrowserModule
