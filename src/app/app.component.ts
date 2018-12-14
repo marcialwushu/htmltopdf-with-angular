4e75a446e9c8a15a6811efb6ecaa2b4773a10931
marcialwushu <marcialwushu@gmail.com>
2018-12-14 17:21:06 -0200

    jsPDF npm package added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HtmlPDF';
+
+  @ViewChild('content') content: ElementRef;
+  public downloadPDF() {
+
+    let doc = new jsPDF('l', 'mm', 'a4');
+
+    let specialElementHandlers = {
+      '#editor': function(element, renderer) {
+        return true;
+      }
+    };
+
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test.pdf');
+  }
 }
